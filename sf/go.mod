eed737ae925c2de78cb3b5bede500d4902756000
jackbisceglia <jackbisceglia2000@gmail.com>
2022-06-18 23:56:42 -0700

    feat: psql api integrations

--- sf/go.mod
+++ sf/go.mod
@@ -4,5 +4,6 @@ go 1.18
 
 require (
 	github.com/gorilla/mux v1.8.0
+	github.com/joho/godotenv v1.4.0
 	github.com/lib/pq v1.10.6
 )
--- sf/routes/user.go
+++ sf/routes/user.go
@@ -1,7 +1,9 @@
 package routes
 
 import (
+	"database/sql"
 	"encoding/json"
+	"fmt"
 	"net/http"
 
 	crud "github.com/jackbisceglia/internship-tracker/crud"
@@ -14,21 +16,31 @@ type Response struct {
 }
 
 type SignUpData struct {
-	emailAddress string
-	listPreferences []string
+	EmailAddress string `json:"emailAddress"`
+	ListPreferences []string `json:"listPreferences"`
 }
 
-func UserRoutes(router *mux.Router, db string) {
+func UserRoutes(router *mux.Router, db *sql.DB) {
 	// Pass db instance to UserCrud to get back User Crud Functions
 	GetUsersByList, InsertUser := crud.UserCrud(db)
 
 	// GET BY LIST TYPE
 	router.HandleFunc("/{listType}", func(w http.ResponseWriter, r *http.Request) {
 		listType := mux.Vars(r)["listType"]
+		
+		users := GetUsersByList(listType)
 
-		GetUsersByList(listType)
+		fmt.Printf("%v", users)
 
+		w.Header().Set("Content-Type", "application/json")
+		res, err := json.Marshal(users)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
 		w.WriteHeader(http.StatusOK)
+		w.Write(res)
+
 	}).Methods("GET")
 
 	// POST USER INTO DB
@@ -39,12 +51,13 @@ func UserRoutes(router *mux.Router, db string) {
 		// Check for errors, and decode JSON into variable typed as struct
 		err := json.NewDecoder(r.Body).Decode(&signUpData)
 		if err != nil {
+			fmt.Printf("error here")
 			http.Error(w, err.Error(), http.StatusBadRequest)
 			return
 		}
 
 		// Insert user into database
-		success := InsertUser(signUpData.emailAddress, signUpData.listPreferences)
+		success := InsertUser(signUpData.EmailAddress, signUpData.ListPreferences)
 
 		// Set Response Type on Header
 		w.Header().Set("Content-Type", "application/json")
