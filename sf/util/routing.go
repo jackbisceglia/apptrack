222a1851fa1ab2265c8496309f65913e60a37935
jackbisceglia <jackbisceglia2000@gmail.com>
2022-07-10 18:30:34 -0700

    feat: ValidateUserRequest function to check incoming API key against env var

--- sf/util/routing.go
+++ sf/util/routing.go
@@ -2,9 +2,14 @@ package util
 
 import (
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
 )
+
+func ValidateUserRequest(keyFromRequest string) bool {
+	return keyFromRequest != "" && keyFromRequest == os.Getenv("USERS_API_KEY")
+}
 
 func RouterUtils(router *mux.Router) func([]string, func(http.ResponseWriter, *http.Request), string) {
 	HandleMultipleRoutes := func(routes []string, handler func(http.ResponseWriter, *http.Request), method string) {
