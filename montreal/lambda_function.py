3b1a5238f649e7d5639661fc179fcdea3964ce1d
Nabil <nabilbaugher@gmail.com>
2022-07-17 22:49:59 -0700

    separate dev and prod apis (#94)

--- montreal/lambda_function.py
+++ montreal/lambda_function.py
@@ -12,37 +12,46 @@ from dotenv import load_dotenv
 USERS_URL = None
 POSTINGS_URL = None
 
+
 def make_global_urls():
     global USERS_URL, POSTINGS_URL
     load_dotenv()
-    USERS_URL = 'https://internship-tracker-production.up.railway.app/users/' + os.getenv('USERS_API_KEY')
-    POSTINGS_URL = 'https://internship-tracker-production.up.railway.app/postings'
+    BASE_URL = os.getenv('URL')
+    USERS_URL = BASE_URL + '/users/' + os.getenv('USERS_API_KEY')
+    POSTINGS_URL = BASE_URL + '/postings'
+
 
 def lambda_handler(event=None, context=None):
     make_global_urls()
     # fetch old postings from database
     intern_db_postings = get_postings_from_db(is_intern=True)
     new_grad_db_postings = get_postings_from_db(is_intern=False)
-    
+
     # fetch all postings from web
     intern_web_postings = get_postings_from_web(is_intern=True)
     new_grad_web_postings = get_postings_from_web(is_intern=False)
-    
+
     # calculate which postings are new
-    new_intern_postings = get_new_postings(intern_db_postings, intern_web_postings)
-    new_new_grad_postings = get_new_postings(new_grad_db_postings, new_grad_web_postings)
-    
-    new_intern_postings_exist = new_intern_postings is not None and len(new_intern_postings) != 0
-    new_new_grad_postings_exist = new_new_grad_postings is not None and len(new_new_grad_postings) != 0
-    
+    new_intern_postings = get_new_postings(
+        intern_db_postings, intern_web_postings)
+    new_new_grad_postings = get_new_postings(
+        new_grad_db_postings, new_grad_web_postings)
+
+    new_intern_postings_exist = new_intern_postings is not None and len(
+        new_intern_postings) != 0
+    new_new_grad_postings_exist = new_new_grad_postings is not None and len(
+        new_new_grad_postings) != 0
+
     if new_intern_postings_exist and new_new_grad_postings_exist:
         all_recipients = get_all_recipients_from_db()
         intern_recipients = filter_recipients(all_recipients, is_intern=True)
-        new_grad_recipients = filter_recipients(all_recipients, is_intern=False)
+        new_grad_recipients = filter_recipients(
+            all_recipients, is_intern=False)
     elif new_intern_postings_exist:
         intern_recipients = get_recipients_from_db_by_is_intern(is_intern=True)
     else:
-        new_grad_recipients = get_recipients_from_db_by_is_intern(is_intern=False)
+        new_grad_recipients = get_recipients_from_db_by_is_intern(
+            is_intern=False)
 
     # send out new intern postings if there are any
     if new_intern_postings_exist:
@@ -52,7 +61,7 @@ def lambda_handler(event=None, context=None):
         update_db_postings(new_intern_postings, is_intern=False)
     else:
         print('No new intern postings!')
-        
+
     # send out new new grad postings if there are any
     if new_new_grad_postings_exist:
         new_grad_table = build_email_html(new_new_grad_postings)
@@ -66,18 +75,20 @@ def lambda_handler(event=None, context=None):
         'statusCode': 200,
         'body': json.dumps('Postings sent!')
     }
-    
+
+
 def get_all_recipients_from_db():
     GET_URL = USERS_URL
-    
+
     response = requests.get(GET_URL)
     if not response.ok:
         print('Error fetching all recipients:', response.status_code)
         return None
-        
+
     data = response.json()
     return data
-    
+
+
 def get_recipients_from_db_by_is_intern(is_intern):
     GET_URL = USERS_URL
     GET_URL += '/intern' if is_intern else '/newgrad'
@@ -85,11 +96,13 @@ def get_recipients_from_db_by_is_intern(is_intern):
     response = requests.get(GET_URL)
     if not response.ok:
         target_group = 'intern' if is_intern else 'new grad'
-        print('Error fetching', target_group, 'recipients:', response.status_code)
+        print('Error fetching', target_group,
+              'recipients:', response.status_code)
         return None
-    
+
     data = response.json()
     return data
+
 
 def filter_recipients(recipients, is_intern):
     result = []
@@ -102,26 +115,29 @@ def filter_recipients(recipients, is_intern):
             result.append(recipient)
     return result
 
+
 def get_postings_from_db(is_intern):
     GET_URL = POSTINGS_URL
-    
+
     response = requests.get(GET_URL)
     if not response.ok:
         print('Error fetching postings from database:', response.status_code)
         return None
-    
+
     data = response.json()
     if is_intern:
         return data['InternPosts']
     else:
         return data['NewGradPosts']
 
+
 def get_postings_from_web(is_intern):
     PITTCSC_INTERNSHIP_URL = 'https://github.com/pittcsc/Summer2023-Internships'
     CODERQUAD_NEW_GRAD_URL = 'https://github.com/coderQuad/New-Grad-Positions-2023'
 
-    page = requests.get(PITTCSC_INTERNSHIP_URL if is_intern else CODERQUAD_NEW_GRAD_URL)
-    
+    page = requests.get(
+        PITTCSC_INTERNSHIP_URL if is_intern else CODERQUAD_NEW_GRAD_URL)
+
     data = []
     soup = BeautifulSoup(page.content, "html.parser")
     table = soup.find('table')
@@ -133,11 +149,11 @@ def get_postings_from_web(is_intern):
         cols_text = [ele.text.strip() for ele in cols]
         try:
             url = cols[0].find('a')['href']
-        except: # no url --> posting closed
+        except:  # no url --> posting closed
             continue
         cols_text.insert(1, url)
         data.append(cols_text)
-    
+
     # transform data to json style
     result = []
 
@@ -153,38 +169,44 @@ def get_postings_from_web(is_intern):
 
     return result
 
+
 def get_new_postings(db_postings_data, web_postings_data):
     # key: email, value: json posting
-    db_map = {posting['url'] : posting for posting in db_postings_data}
-    web_map = {posting['url'] : posting for posting in web_postings_data}
-    
+    db_map = {posting['url']: posting for posting in db_postings_data}
+    web_map = {posting['url']: posting for posting in web_postings_data}
+
     # calculate set difference to get new emails
     new_urls = set(web_map.keys()).difference(set(db_map.keys()))
-    
+
     result = []
     for url in new_urls:
         result.append(web_map[url])
     return result
 
+
 def update_db_postings(new_postings, is_intern):
     POST_URL = POSTINGS_URL
     response = requests.post(POST_URL, json=new_postings)
-    print('Attempting to update database with new '+ ('intern ' if is_intern else 'new grad ') +'postings...')
+    print('Attempting to update database with new ' +
+          ('intern ' if is_intern else 'new grad ') + 'postings...')
     print(response)
+
 
 def send_mail(recipients, email_html, email_title):
     # setup
     FROM_EMAIL = 'jobs@apptrack.tech'
-    mailjet = Client(auth=(os.getenv("MAILJET_API_KEY"), os.getenv("MAILJET_API_SECRET_KEY")), version='v3.1')
-    
+    mailjet = Client(auth=(os.getenv("MAILJET_API_KEY"), os.getenv(
+        "MAILJET_API_SECRET_KEY")), version='v3.1')
+
     if recipients is None:
         print('no recipients')
         return
-    
+
     # send data
     for recipient in recipients:
-        unsub_link = '<div class="unsub_link"><a href="https://apptrack.tech/unsubscribe/'+recipient['id']+'">Unsubscribe</a></div></div></body></html>'
-        errors = [] 
+        unsub_link = '<div class="unsub_link"><a href="https://apptrack.tech/unsubscribe/' + \
+            recipient['id']+'">Unsubscribe</a></div></div></body></html>'
+        errors = []
         data = {
             'Messages': [
                 {
@@ -210,24 +232,28 @@ def send_mail(recipients, email_html, email_title):
         if int(result.status_code) != 200:
             errors.append((result.status_code, recipient['emailAddress']))
 
+
 def build_email_title(postings):
     if len(postings) == 1:
         return 'New job posting from ' + postings[0]['company'] + '!'
-    
+
     company_names = [posting['company'] for posting in postings]
     if len(postings) == 2:
         company_names_str = company_names[0] + ' and ' + company_names[1]
     elif len(postings) == 3:
-        company_names_str = ', '.join(company_names[:-1]) + ', and ' + company_names[-1] 
+        company_names_str = ', '.join(
+            company_names[:-1]) + ', and ' + company_names[-1]
     else:
         company_names_str = ', '.join(company_names[:3]) + ', and more'
     return 'New job postings from ' + company_names_str + '!'
+
 
 def build_email_html_original(postings):
     table = '<html><head></head><table><thead><tr><th>Name</th><th>Location</th><th>Notes</th></tr></thead><tbody>'
     for posting in postings:
         row = '<tr>'
-        row += '<td><a href='+posting['url']+' rel="nofollow">'+posting['company']+'</a></td>'
+        row += '<td><a href=' + \
+            posting['url']+' rel="nofollow">'+posting['company']+'</a></td>'
         row += '<td>'+posting['location']+'</td>'
         row += '<td>'+posting['notes']+'</td>'
         row += '</tr>'
@@ -235,11 +261,13 @@ def build_email_html_original(postings):
     table += '</tbody></table>'
     return table
 
+
 def build_email_html(postings):
     table = '<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"/><meta http-equiv="X-UA-Compatible" content="IE=edge"/><meta name="viewport" content="width=device-width, initial-scale=1.0"/><title>APPTRACK Daily Update</title><style>@import url("https://fonts.googleapis.com/css2?family=Sen:wght@400;700;800&display=swap");body{margin: 0;background-color: #f5f5f4;color: #292524;font-family: "Sen", sans-serif;}h1{text-align: center;margin: 0;padding-top: 40px;padding-bottom: 40px;font-size:48px;}h3{margin: 0;}p{margin-top: 10px;margin-bottom: 15px;}a{color: inherit !important;text-decoration: none!important;}.wrapper{max-width: 500px;margin: 0 auto;padding: 0 10px;}.post{display: table;width: 100%;margin-bottom: 40px;border-bottom: 2px solid #78716c;}.posting_link{display: table-cell;text-align: right;vertical-align: middle;color: #ef4444 !important;text-decoration: underline;min-width: 62px;font-size: 18px;}.posting_details{display: table-cell;width: 100%;}.unsub_link{text-align: center;text-decoration: underline;padding-bottom: 40px;}</style></head><body><div class="wrapper"><h1><a href="https://apptrack.tech" text-decoration: none;>APPTRACK</a></h1>'
     for posting in postings:
         table += build_posting_html(posting)
     return table
+
 
 def build_posting_html(posting):
     result = '<div class="post"><div class="posting_details"><h3>'
